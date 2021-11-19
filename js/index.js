a82da0d282cc187e1ce0fddb11f439ca0326a6ef
YunYouJun <me@yunyoujun.cn>
2021-11-19 19:19:25 +0800

    :sparkles: use dataset instead of link href

--- js/index.js
+++ js/index.js
@@ -1,37 +1,37 @@
-'use strict';
+"use strict";
 
 const CommonTags = {
-  registerTabsTag: function() {
+  registerTabsTag: function () {
     // Binding `nav-tabs` & `tab-content` by real time permalink changing.
-    document.querySelectorAll('.tabs ul.nav-tabs .tab').forEach(element => {
-      element.addEventListener('click', event => {
+    document.querySelectorAll(".tabs ul.nav-tabs .tab").forEach((element) => {
+      element.addEventListener("click", (event) => {
         event.preventDefault();
         // Prevent selected tab to select again.
-        if (element.classList.contains('active')) return;
+        if (element.classList.contains("active")) return;
         // Add & Remove active class on `nav-tabs` & `tab-content`.
-        [...element.parentNode.children].forEach(target => {
-          target.classList.toggle('active', target === element);
+        [...element.parentNode.children].forEach((target) => {
+          target.classList.toggle("active", target === element);
         });
         // https://stackoverflow.com/questions/20306204/using-queryselector-with-ids-that-are-numbers
         const tActive = document.getElementById(
-          element.querySelector('a').getAttribute('href').replace('#', '')
+          element.querySelector("a").dataset.target
         );
-        [...tActive.parentNode.children].forEach(target => {
-          target.classList.toggle('active', target === tActive);
+        [...tActive.parentNode.children].forEach((target) => {
+          target.classList.toggle("active", target === tActive);
         });
         // Trigger event
         tActive.dispatchEvent(
-          new Event('tabs:click', {
-            bubbles: true
+          new Event("tabs:click", {
+            bubbles: true,
           })
         );
       });
     });
 
-    window.dispatchEvent(new Event('tabs:register'));
-  }
+    window.dispatchEvent(new Event("tabs:register"));
+  },
 };
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   CommonTags.registerTabsTag();
 });
