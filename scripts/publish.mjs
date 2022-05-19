1e598fc61277ecd138c77608acd2f54785d7453d
YunYouJun <me@yunyoujun.cn>
2022-05-20 02:59:28 +0800

    ci: add ci publish & release

---
+++ scripts/publish.mjs
@@ -0,0 +1,4 @@
+
+import { $ } from 'zx'
+
+await $`npx pnpm publish --access public --no-git-checks`
---
+++ scripts/release.mjs
@@ -0,0 +1,12 @@
+import { execSync } from 'child_process'
+import { $, fs } from 'zx'
+
+execSync('npx bumpp package.json', { stdio: 'inherit' })
+
+const { version } = await fs.readJSON('package.json')
+
+await $`git add .`
+await $`git commit -m "chore: release v${version}"`
+await $`git tag v${version}`
+await $`git push`
+await $`git push origin --tags`
